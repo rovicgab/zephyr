886f3214b835d553b046ea16afc05e6bcac3cd77
Em <104334306+JohnM-Reyes24@users.noreply.github.com>
2022-09-26 23:06:27 +0800

    test

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('jwt_helper');
+$autoload['helper'] = array();
 
 /*
 | -------------------------------------------------------------------
